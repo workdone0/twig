2f5a212b85be47884f6d2d51fb570674b9b477f6
workdone0 <sk246903@gmail.com>
2025-12-19 14:53:49 +0530

    changing homes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://workdone0.github.io",
-  base: "/twig",
+  site: "https://twig.wtf",
   integrations: [sitemap()],
 });
