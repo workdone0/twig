a1363cf0e24fadb64f10a710e9c76c4c9d5ac918
workdone0 <sk246903@gmail.com>
2025-12-19 12:26:50 +0530

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://workdone0.github.io",
-    base: "/twig",
+  site: "https://workdone0.github.io",
+  base: "/twig",
+  integrations: [sitemap()],
 });
